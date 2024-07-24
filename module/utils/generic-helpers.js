a09b9968c58f63ab1d644f6e61de2d574dfaa8ea
Highmongrel <47551519+Highmongrel@users.noreply.github.com>
2024-07-24 02:49:04 -0700

    Add Normal distribution
    generates normal between 0-1

--- module/utils/generic-helpers.js
+++ module/utils/generic-helpers.js
@@ -10,6 +10,22 @@ export function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
 }
 
+function randn_bm() {        //Box–Muller transform from Stack Overflow
+    let u = 0, v = 0;
+    while(u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+    while(v === 0) v = Math.random();
+    let num = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+    num = num / 10.0 + 0.5; // Translate to 0 -> 1
+    if (num > 1 || num < 0) return randn_bm() // resample between 0 and 1
+    return num
+}
+
+function getRandomNormIntInclusive(min, max) {        //Made seperate to not break anything else calling RandIntInclusive
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(randn_bm() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
+
 export function lpad(value, padding) {
     var zeroes = new Array(padding+1).join("0");
     return (zeroes + value).slice(-padding);
